fa1cf62795a74bd3020d2bf80f42745ad3645c59
Robyn <78262868+Robsness23@users.noreply.github.com>
2021-06-13 16:00:46 +0100

    Initial commit - added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Dictionary from "./Dictionary";
+
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Dictionary
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <small className="App-footer">
+        Coded by Robyn Smith
+      </small>
+      </div>
     </div>
   );
 }
-
-export default App;
